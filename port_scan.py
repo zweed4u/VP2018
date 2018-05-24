3cdb2c36eb4ece60d07dd370b01b79c170b58f56
Zachary Weeden <zdw7287@rit.edu>
2018-05-23 23:27:43 -0400

    Finding a service - socket basics

--- port_scan.py
+++ port_scan.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import socket
-s = socket.socket()
+for port in range(9999):
+    s = socket.socket()
+    s.settimeout(2)
+    #port = int(input('Port: '))  # manual input for specific port
 
-# Exception handling for gai errors
-try:
-    s.connect(('ad.samsclass.info', 22))  # connect to server:22
-    print(s.recv(1024))  # receives data up to 1024 chars from server
-    s.close()
-except socket.error as err:
-    print(err)
+    # Exception handling for gai errors
+    try:
+        s.connect(('ad.samsclass.info', port))  # connect to server:22
+        banner = s.recv(1024)
+        print(f'Port #{port}: {banner}')  # receives data up to 1024 chars from server
+        s.close()
+        if 'congratulations' in banner.decode('utf8').lower():
+            break
+    except socket.error as err:
+        print(f'Port #{port}: {err}')
